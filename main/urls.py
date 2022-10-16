8126852983a0dba7550aaee779057ff3efa9d30b
Vasyl Abramovych <vasyl.abramovych@gmail.com>
2022-10-16 12:23:26 -0700

    solved

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<code>/', views.main),
+    path('join/<code>/', views.main),
     path('new_room/', views.new_room),
     path('new_user/', views.new_user),
     path('get_times/', views.get_times),
